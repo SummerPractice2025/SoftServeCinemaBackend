78dbd61f179473596f5fe0869e96ff953bad7538
JeFFlidan <zaveruha678@gmail.com>
2025-06-23 01:10:56 +0300

    Implemented GET/movies request

--- src/modules/movie/movie.module.ts
+++ src/modules/movie/movie.module.ts
@@ -2,10 +2,11 @@ import { Module, forwardRef } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
 import { TmdbModule } from '../tmdb/tmdb.module';
+import { MoviesController } from './movies.controller';
 
 @Module({
   imports: [forwardRef(() => TmdbModule)],
-  controllers: [MovieController],
+  controllers: [MovieController, MoviesController],
   providers: [MovieService],
 })
 export class MovieModule {}
