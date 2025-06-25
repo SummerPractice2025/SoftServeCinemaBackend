b69c55176dd55e9a246fa48450b3959c9b773131
Daniel04203 <danilomakaoov@gmail.com>
2025-06-25 08:31:50 +0300

    Added all the necessary checks, validations, etc.

--- src/modules/movie/movie.module.ts
+++ src/modules/movie/movie.module.ts
@@ -2,9 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
 import { TmdbModule } from '../tmdb/tmdb.module';
+import { AgeRatesModule } from '../age-rates/age-rates.module';
 
 @Module({
-  imports: [forwardRef(() => TmdbModule)],
+  imports: [forwardRef(() => TmdbModule), AgeRatesModule],
   controllers: [MovieController],
   providers: [MovieService],
 })
