45b2b60c0b854d770df6e45b8e35e6362c0e395b
Daniel04203 <danilomakaoov@gmail.com>
2025-06-26 12:41:03 +0300

    Added backend / frontend connection

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('SSienma')
     .setDescription('The SSinema project API description')
