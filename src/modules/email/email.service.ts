8d82d78e9bcfcd2567f4732bb319362343503306
JeFFlidan <zaveruha678@gmail.com>
2025-07-04 00:14:03 +0300

    Added sendMail method

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  async sendEmail(email: string, subject: string, html: string) {
+    try {
+      await this.transporter.sendMail({
+        from: process.env.EMAIL,
+        to: email,
+        subject: subject,
+        html: html,
+      });
+    } catch (e) {
+      console.error(`Failed to send email! ${e}`);
+    }
+  }
+}
